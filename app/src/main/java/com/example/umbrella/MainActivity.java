87208e2ae017f477f51e558b93e708c45a72af58
hyeonjichu <guswl8280@naver.com>
2020-08-24 01:47:58 +0900

    어플 디자인

--- app/src/main/java/com/example/umbrella/MainActivity.java
+++ app/src/main/java/com/example/umbrella/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.umbrella;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-class MainActivity extends Activity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i = new Intent(this, userActivity.class);
-        startActivity(i);
+        Intent intent = new Intent(this,userActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/umbrella/userActivity.java
+++ app/src/main/java/com/example/umbrella/userActivity.java
@@ -7,5 +7,6 @@ public class userActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_activity);
     }
 }
